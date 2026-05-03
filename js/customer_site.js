cb89f56a5f576d356f7f231f2a53fde3a1d8d2fb
tonlylove9 <Tonlylove9@gmail.com>
2026-05-03 14:02:52 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,6 +2,22 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
+    },
+    360: {
+        api: 'https://360zy.com/api.php/provide/vod/',
+        name: '360资源',
+    },
+    feifan: {
+        api: 'https://cj.ffzyapi.com/api.php/provide/vod/',
+        name: '非凡资源',
+    },
+    liangzi: {
+        api: 'https://cj.lziapi.com/api.php/provide/vod/',
+        name: '量子资源',
+    },
+    suoni: {
+        api: 'https://suoniapi.com/api.php/provide/vod/',
+        name: '索尼资源',
     }
 };
 
